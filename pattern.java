97aa7d3fc371520bdd6ce96f1f7736ae5032379f
Abhiraj Singh <abhi973686@gmail.com>
2018-08-18 17:50:49 +0530

    JAVA assignment 1

---
+++ pattern.java
@@ -0,0 +1,15 @@
+class pattern
+{
+	public static void main(String[] args) 
+	{
+		int a,b;	
+		for(a=1;a<6;a++)
+		{
+			for(b=0;b<a;b++)
+			{
+				System.out.print("*");
+			}
+			System.out.print("\n");
+		}	
+	}
+}
